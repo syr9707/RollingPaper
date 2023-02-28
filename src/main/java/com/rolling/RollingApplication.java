735f202e33f74abb4485d9a9fdfac305800ae327
syr9707 <yrshin9771@gmail.com>
2023-02-28 22:16:29 +0900

    first commit

---
+++ src/main/java/com/rolling/RollingApplication.java
@@ -0,0 +1,13 @@
+package com.rolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingApplication.class, args);
+	}
+
+}
