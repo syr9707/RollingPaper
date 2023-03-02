069c03d12ca83ca0200ea61bbba2e0f0f4ea9145
saehim <saeilee.vxv@gmail.com>
2023-03-02 10:00:28 +0900

    Feat (Board) : board repository interface extends 구현

--- src/main/java/com/rolling/domain/board/BoardRepository.java
+++ src/main/java/com/rolling/domain/board/BoardRepository.java
@@ -1,2 +1,7 @@
-package com.rolling.domain.board;public interface BoardRepository {
+package com.rolling.domain.board;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+
 }
