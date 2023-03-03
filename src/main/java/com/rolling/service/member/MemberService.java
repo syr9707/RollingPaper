c6b0220c7ee2f5e49890096b7de199c1dd7742cb
syr9707 <yrshin9771@gmail.com>
2023-03-03 14:37:14 +0900

    Feat: 회원정보 수정 Service 생성
    - Member Update
    
    Related to: #4

--- src/main/java/com/rolling/service/member/MemberService.java
+++ src/main/java/com/rolling/service/member/MemberService.java
@@ -1,9 +1,12 @@
 package com.rolling.service.member;
 
+import com.rolling.config.SecurityUtil;
 import com.rolling.domain.member.Member;
 import com.rolling.domain.member.MemberRepository;
 import com.rolling.web.dto.member.MemberSaveRequestDto;
+import com.rolling.web.dto.member.MemberUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     // 회원 저장
     @Transactional
@@ -29,6 +34,18 @@ public class MemberService {
         if(memberRepository.findByEmail(memberSaveRequestDto.getEmail()).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 회원입니다.");
         }
+    }
+
+    @Transactional
+    public Long update(Long memberId, MemberUpdateRequestDto memberUpdateRequestDto) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new IllegalArgumentException("해당 회원이 없습니다. memberId = " + memberId)
+        );
+
+        member.updatePassword(passwordEncoder, memberUpdateRequestDto.getPassword());
+        member.updateNickname(memberUpdateRequestDto.getNickname());
+
+        return memberId;
     }
 
 }
