dc45d12d2925b99652ecafd42dca53a7ea3ef646
saehim <saeilee.vxv@gmail.com>
2023-03-04 15:38:34 +0900

    Feat (board) : controller, service 초반 작업
    - service와 controller를 생성했다.
    - 회원가입시 보드는 자동으로 1개 생성되고 추가 생성할 계획이 없기 때문에, 딱히 추가로 해줘야 할 서비스가 보이지 않는다.
    - 핵심 로직 중 하나인 메시지 기능을 추가한 후 다시 돌아와 필요한 서비스와 컨트롤러를 완성해보자
    
    related to #2

--- src/main/java/com/rolling/service/board/BoardService.java
+++ src/main/java/com/rolling/service/board/BoardService.java
@@ -1,2 +1,33 @@
-package com.rolling.service.board;public class BoardService {
+package com.rolling.service.board;
+
+import com.rolling.domain.board.Board;
+import com.rolling.domain.board.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+    @Autowired
+    private BoardRepository boardRepository;
+
+    // 회원 가입시 자동 1개 생성
+    public void create(Board board) {
+        boardRepository.save(board);
+    }
+
+    public void delete(Board board) {
+        boardRepository.delete(board);
+    }
+
+    public List<Board> show() {
+        return boardRepository.findAll();
+    }
+
+    public Board fileABoard(String memberId) {
+        return boardRepository.findAll().stream()
+                .filter(e -> e.getMemberId().equals(memberId))
+                .findFirst().get();
+    }
 }
