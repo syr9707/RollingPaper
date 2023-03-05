b94ef6094210016bda3132ddb9aaad5e56a2d30f
saehim <saeilee.vxv@gmail.com>
2023-03-05 21:18:27 +0900

    Feat (meassage) : DTO, view 초안
    - DTO 초안, 이걸 어떻게 써야하는지...?
    - view 페이지 초안 (테스트 아직 안해봤다)
    
    related to #3

--- src/main/java/com/rolling/dto/board/BoardDto.java
+++ src/main/java/com/rolling/dto/board/BoardDto.java
@@ -1,2 +1,19 @@
-package com.rolling.dto.board;public class boardDto {
+package com.rolling.dto.board;
+
+import com.rolling.domain.board.Board;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BoardDto {
+    private String boardTitle;
+    private String boardContent;
+
+    public Board toEntity(){
+        return Board.builder()
+                .boardTitle(boardTitle)
+                .boardContent(boardContent)
+                .build();
+    }
 }
