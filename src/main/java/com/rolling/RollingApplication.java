9b8c32909ec778836dcb9b8b762770ae4836c368
saehim <saeilee.vxv@gmail.com>
2023-03-05 21:17:48 +0900

    Test (message) : 메세지 삽입, 등록 시간 테스트
    - 메시지 DB 삽입 테스트
    - 등록시간 테스트
    
    related to #3

--- src/main/java/com/rolling/RollingApplication.java
+++ src/main/java/com/rolling/RollingApplication.java
@@ -2,7 +2,9 @@ package com.rolling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RollingApplication {
 
--- src/test/java/com/rolling/domain/message/MessageRepositoryTest.java
+++ src/test/java/com/rolling/domain/message/MessageRepositoryTest.java
@@ -1,4 +1,81 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.rolling.domain.message;
+
+import com.rolling.domain.board.Board;
+import com.rolling.domain.board.BoardRepository;
+import com.rolling.dto.message.MessageDto;
+import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class MessageRepositoryTest {
-  
+    @Autowired
+    MessageRepository messageRepository;
+    @Autowired
+    BoardRepository boardRepository;
+
+    @BeforeEach
+    void init() {
+        boardRepository.save(Board.builder()
+                .boardTitle("test")
+                .boardContent("test con")
+                .memberId("test")
+                .build());
+    }
+
+    @AfterEach
+    void clean() {
+        messageRepository.deleteAll();
+//        boardRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        messageRepository.save(Message.builder()
+                        .board(boardRepository.findById(1L).get())
+                        .messageTitle("테스트 게시글")
+                        .messageContents("테스트 본문")
+                        .writer("jojoldu@gmail.com")
+                        .messageState(MessageState.OPEN)
+                        .build());
+
+        //when
+        List<Message> postsList = messageRepository.findAll();
+
+        //then
+        Message posts = postsList.get(0);
+        assertThat(posts.getMessageTitle()).isEqualTo("테스트 게시글");
+        assertThat(posts.getMessageContents()).isEqualTo("테스트 본문");
+    }
+
+    @Test
+    public void BaseTimeEntity_등록 () {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        messageRepository.save(Message.builder()
+                .messageTitle("테스트 게시글")
+                .messageContents("테스트 본문")
+                .writer("jojoldu@gmail.com")
+                .messageState(MessageState.OPEN)
+                .build());
+        //when
+        List<Message> postsList = messageRepository.findAll();
+
+        //then
+        Message posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
+    }
+
 }
