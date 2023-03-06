45c28f57c3113f69b26c7503d226b7d9209f06d1
syr9707 <yrshin9771@gmail.com>
2023-03-06 19:18:05 +0900

    Feat: 회원가입 & 로그인 Controller 구현
    - 회원가입 : /signUp
    - 로그인: /login
    
    Related to: #4

--- src/main/java/com/rolling/web/MemberController.java
+++ src/main/java/com/rolling/web/MemberController.java
@@ -1,2 +1,55 @@
-package com.rolling.web;public class MemberController {
+package com.rolling.web;
+
+import com.rolling.domain.member.Member;
+import com.rolling.domain.member.MemberRepository;
+import com.rolling.service.member.MemberService;
+import com.rolling.web.dto.member.MemberSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+//@RestController
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+    public final MemberRepository memberRepository;
+
+    @GetMapping("/")
+    public @ResponseBody String index() {
+        return "index 페이지 입니다.";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "member/loginForm";
+    }
+
+//    @PostMapping("/login")
+//    public void login() {
+//
+//    }
+
+    @GetMapping("/signUp")
+    public String signUpForm(Model model) {
+        model.addAttribute("MemberSaveRequestDto", new MemberSaveRequestDto());
+        return "member/signUpForm";
+    }
+
+    @PostMapping("/signUp")
+    public @ResponseBody Long signUp(@Valid  MemberSaveRequestDto memberSaveRequestDto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            throw new IllegalArgumentException("Error");
+        }
+
+        return memberService.saveMember(memberSaveRequestDto);
+    }
+
 }
